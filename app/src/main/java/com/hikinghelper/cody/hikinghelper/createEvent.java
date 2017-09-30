d2e6489e4722eab2b2bd90fd9114ae85c52f4f30
Cody Reandeau <codyreandeau@yahoo.com>
2017-09-30 11:18:12 -0400

    Added new page to create events(create_event). Added form elements to fill out on said page. Added button on events page to send users to this new page. Added background color to guide page.

--- app/src/main/java/com/hikinghelper/cody/hikinghelper/createEvent.java
+++ app/src/main/java/com/hikinghelper/cody/hikinghelper/createEvent.java
@@ -1,34 +1,20 @@
 package com.hikinghelper.cody.hikinghelper;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.widget.Button;
 
-public class event extends AppCompatActivity {
+public class createEvent extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.event);
+        setContentView(R.layout.activity_create_event);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        //Set click event listener
-        //button to send user to appropriate page
-        Button btnGuide=(Button)findViewById(R.id.btnCreate);
-
-        //Send user to create Event page
-        btnGuide.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(event.this, createEvent.class));
-            }
-        });
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
