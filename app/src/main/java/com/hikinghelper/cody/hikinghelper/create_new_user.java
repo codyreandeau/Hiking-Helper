ba6d71371208a8d48924edc1739744aff5cc8bd2
Cody Reandeau <codyreandeau@yahoo.com>
2017-10-05 18:16:12 -0400

    Added new user button on login page. Created create_new_user page. New button on login page will send you to the create_new_user page.

--- app/src/main/java/com/hikinghelper/cody/hikinghelper/create_new_user.java
+++ app/src/main/java/com/hikinghelper/cody/hikinghelper/create_new_user.java
@@ -1,35 +1,23 @@
 package com.hikinghelper.cody.hikinghelper;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.widget.Button;
 
-public class login extends AppCompatActivity {
+public class create_new_user extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_create_new_user);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         //set action bar text
-        getSupportActionBar().setTitle("Login");
-
-        Button btnNewUser=(Button)findViewById(R.id.btnNew);
-
-        //Send user to Login page
-        btnNewUser.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(login.this, create_new_user.class));
-            }
-        });
+        getSupportActionBar().setTitle("Create New User");
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
