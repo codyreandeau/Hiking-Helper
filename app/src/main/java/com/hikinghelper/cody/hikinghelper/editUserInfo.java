173ab7956ae169c90c2ec467a836b4415b1a87d8
Cody Reandeau <codyreandeau@yahoo.com>
2017-10-15 21:51:55 -0400

    added 'edit user info' page. Added update button from the login page to send to the edit user info page.

--- app/src/main/java/com/hikinghelper/cody/hikinghelper/editUserInfo.java
+++ app/src/main/java/com/hikinghelper/cody/hikinghelper/editUserInfo.java
@@ -7,36 +7,23 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.widget.Button;
 
-public class User extends AppCompatActivity {
+public class editUserInfo extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.user);
+        setContentView(R.layout.activity_edit_user_info);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        //set action bar text
-        getSupportActionBar().setTitle("Your Profile");
-
-        Button btnUpdate=(Button)findViewById(R.id.btnUpdate);
-
-        //Send user to Update User Info Page page
-        btnUpdate.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(User.this, editUserInfo.class));
-            }
-        });
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(User.this, Home.class));
+                startActivity(new Intent(editUserInfo.this, User.class));
             }
         });
     }
+
 }
