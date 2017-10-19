a9c7ee2ad30b013980676c00a3879cd4ad35a1e8
Cody Reandeau <codyreandeau@yahoo.com>
2017-10-18 21:24:14 -0400

    php files removed from repo and added to server. Made necessary adjustments in urls via login.java and create_new_user.java to connect to 000webhost server. Connection established and login and registration is working.

--- app/src/main/java/com/hikinghelper/cody/hikinghelper/create_new_user.java
+++ app/src/main/java/com/hikinghelper/cody/hikinghelper/create_new_user.java
@@ -7,8 +7,31 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class create_new_user extends AppCompatActivity {
+
+    private EditText username, password, email;
+    private Button register;
+    private RequestQueue requestQueue;
+    private static final String URL = "https://hikinghelper.000webhostapp.com/connect/user_register.php";
+    private StringRequest request;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +39,54 @@ public class create_new_user extends AppCompatActivity {
         setContentView(R.layout.activity_create_new_user);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        username = (EditText) findViewById(R.id.txtUsername);
+        password = (EditText) findViewById(R.id.txtPassword);
+        email = (EditText) findViewById(R.id.txtEmail);
+        register = (Button) findViewById(R.id.btnCreate);
+
+        requestQueue = Volley.newRequestQueue(this);
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                request = new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject jsonObject = new JSONObject(response);
+                            if(jsonObject.names().get(0).equals("success")){
+                                Toast.makeText(getApplicationContext(),"SUCCESS "+jsonObject.getString("success"),Toast.LENGTH_SHORT).show();
+                                startActivity(new Intent(getApplicationContext(),Home.class));
+                            }else {
+                                Toast.makeText(getApplicationContext(), "Error" +jsonObject.getString("error"), Toast.LENGTH_SHORT).show();
+                            }
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // System.out.println(error.getMessage());
+                    }
+                }){
+                    @Override
+                    protected Map<String, String> getParams() throws AuthFailureError {
+                        HashMap<String,String> hashMap = new HashMap<String, String>();
+                        hashMap.put("username",username.getText().toString());
+                        hashMap.put("password",password.getText().toString());
+                        hashMap.put("email",email.getText().toString());
+
+                        return hashMap;
+                    }
+                };
+
+                requestQueue.add(request);
+            }
+        });
 
         //set action bar text
         getSupportActionBar().setTitle("Create New User");
