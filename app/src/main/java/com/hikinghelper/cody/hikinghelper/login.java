3b63ff18dc222d37d29f2af0c5c41a42d6248bcd
Cody Reandeau <codyreandeau@yahoo.com>
2017-10-19 22:17:30 -0400

    Removed floating action button from login page. The home screen is now the login page.

--- app/src/main/java/com/hikinghelper/cody/hikinghelper/login.java
+++ app/src/main/java/com/hikinghelper/cody/hikinghelper/login.java
@@ -101,13 +101,5 @@ public class login extends AppCompatActivity {
             }
         });
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(login.this, Home.class));
-            }
-        });
-
     }
 }
