d3aac85d7c2cfa8f222b39659f6abdd9ab423433
Cody Reandeau <codyreandeau@yahoo.com>
2017-10-25 23:00:39 -0400

    James merged xml content on edit user info page with mine, he also merged his code with mine in editUserInfo.java. Photo upload feature is working.

--- app/src/main/java/com/hikinghelper/cody/hikinghelper/editUserInfo.java
+++ app/src/main/java/com/hikinghelper/cody/hikinghelper/editUserInfo.java
@@ -1,7 +1,9 @@
 package com.hikinghelper.cody.hikinghelper;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -9,11 +11,16 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class editUserInfo extends AppCompatActivity {
 
     EditText firstName, age, experience, aboutMe;
+    ImageView imageView3;
+    Button button;
+    private static final int PICK_IMAGE = 100;
+    Uri imageUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -66,6 +73,35 @@ public class editUserInfo extends AppCompatActivity {
                 startActivity(new Intent(editUserInfo.this, User.class));
             }
         });
+
+
+        imageView3 = (ImageView)findViewById(R.id.imageView3);
+        button = (Button)findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openGallery();
+            }
+        });}
+
+    private void openGallery(){
+        Intent gallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+        startActivityForResult(gallery, PICK_IMAGE);
     }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode,resultCode,data);
+        if (requestCode == PICK_IMAGE){//requestCode == RESULT_OK && should be here but then it gives me an always false error
+            imageUri=data.getData();
+            imageView3.setImageURI(imageUri);
+
+        }
+
+
+    }
+
+
+
 
 }
