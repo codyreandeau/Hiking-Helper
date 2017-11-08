f6e4edbead84af6db8272c0f2b8c147c00345fef
Cody Reandeau <codyreandeau@yahoo.com>
2017-11-08 14:44:32 -0500

    Wrote php script to interact with mountains table. Mountain Searh page is currently getting a response from the database. Working on fetching data now.

--- app/src/main/java/com/hikinghelper/cody/hikinghelper/MountainSearch.java
+++ app/src/main/java/com/hikinghelper/cody/hikinghelper/MountainSearch.java
@@ -7,8 +7,33 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Toast;
+import android.widget.SearchView;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MountainSearch extends AppCompatActivity {
+
+    private SearchView mountainName;
+    private Button searchMountain;
+    private RequestQueue requestQueue;
+    private static final String URL = "https://hikinghelper.000webhostapp.com/connect/mountains.php";
+    private StringRequest request;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,17 +42,63 @@ public class MountainSearch extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        //set action bar text
-        getSupportActionBar().setTitle("Mountain Search");
+        mountainName = (SearchView) findViewById(R.id.mountainSearch);
+        searchMountain = (Button) findViewById(R.id.btnSearchMount);
 
-        //Send user back to the home page
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        requestQueue = Volley.newRequestQueue(this);
+
+        requestQueue = Volley.newRequestQueue(this);
+
+        searchMountain.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(MountainSearch.this, Home.class));
+
+                //Validate user in the database
+                request = new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject jsonObject = new JSONObject(response);
+                            if (jsonObject.names().get(0).equals("success")) {
+                                Toast.makeText(getApplicationContext(), "SUCCESS " + jsonObject.getString("success"), Toast.LENGTH_SHORT).show();
+                            } else {
+                                Toast.makeText(getApplicationContext(), "Error" + jsonObject.getString("error"), Toast.LENGTH_SHORT).show();
+                            }
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // System.out.println(error.getMessage());
+                    }
+                }) {
+                    @Override
+                    protected Map<String, String> getParams() throws AuthFailureError {
+                        HashMap<String, String> hashMap = new HashMap<String, String>();
+                        hashMap.put("mountainName", mountainName.getQuery().toString());
+                        return hashMap;
+                    }
+                };
+                requestQueue.add(request);
             }
         });
+
+
+        //set action bar text
+    getSupportActionBar().setTitle("Mountain Search");
+
+    //Send user back to the home page
+    FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            startActivity(new Intent(MountainSearch.this, Home.class));
+        }
+    });
+
     }
 
 }
