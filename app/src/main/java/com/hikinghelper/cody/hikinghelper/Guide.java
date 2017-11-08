1eeba389d78e962b91669ab4aa9e1019a81639d5
Cody Reandeau <codyreandeau@yahoo.com>
2017-11-08 17:38:00 -0500

    These are Quans files. He changed the guide page, added files what_to_pack what_to_wear tips_and_tricks (xml).

--- app/src/main/java/com/hikinghelper/cody/hikinghelper/Guide.java
+++ app/src/main/java/com/hikinghelper/cody/hikinghelper/Guide.java
@@ -7,8 +7,11 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 public class Guide extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,7 +20,43 @@ public class Guide extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        //Send user back to the home page
+        Button hike_but =(Button)findViewById(R.id.hike_but);
+        Button pack_but =(Button)findViewById(R.id.pack_but);
+        Button wear_but =(Button)findViewById(R.id.wear_but);
+        Button tip_but =(Button)findViewById(R.id.tip_but);
+
+
+
+        //Send user to Before_Hike page
+        hike_but.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Guide.this, hike_but.class));
+            }
+        });
+        //Send user to what to wear page
+        wear_but.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Guide.this, wear_but.class));
+            }
+        });
+        //Send user to what to pack page
+        pack_but.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Guide.this, pack_but.class));
+            }
+        });
+        //Send user to tip and trick page
+        tip_but.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Guide.this, tip_but.class));
+            }
+        });
+
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
