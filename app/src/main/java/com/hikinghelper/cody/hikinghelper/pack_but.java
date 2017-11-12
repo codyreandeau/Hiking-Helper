d43d5d8a9879c6116bca3c46aea208fea510302e
Cody Reandeau <codyreandeau@yahoo.com>
2017-11-12 18:03:32 -0500

    Added java files for quans xml files (hike_but, pack_but, tip_but, wear_but).

---
+++ app/src/main/java/com/hikinghelper/cody/hikinghelper/pack_but.java
@@ -0,0 +1,31 @@
+package com.hikinghelper.cody.hikinghelper;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+/**
+ * Created by Cody on 11/12/2017.
+ */
+
+public class pack_but extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.what_to_pack);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+}
