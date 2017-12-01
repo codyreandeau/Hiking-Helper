1276800892c42ad499fa71ae93b6ffa65add3370
Cody Reandeau <codyreandeau@yahoo.com>
2017-11-30 21:25:21 -0500

    changed srcCompat to android:src

--- app/src/main/java/com/hikinghelper/cody/hikinghelper/event.java
+++ app/src/main/java/com/hikinghelper/cody/hikinghelper/event.java
@@ -8,8 +8,33 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ListView;
+import android.widget.ArrayAdapter;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
 
 public class event extends AppCompatActivity {
+
+    private RequestQueue requestQueue;
+    private static final String URL = "https://hikinghelper.000webhostapp.com/connect/fetch_events.php";
+    private StringRequest request;
+    ArrayAdapter<String> adapter;
+    ArrayList<String> items;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +42,10 @@ public class event extends AppCompatActivity {
         setContentView(R.layout.event);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        ListView listView=(ListView)findViewById(R.id.listv);
+        items=new ArrayList<String>();
+        adapter=new ArrayAdapter(this, R.layout.event,R.id.txt,items);
+        listView.setAdapter(adapter);
 
         //set action bar text
         getSupportActionBar().setTitle("Events");
@@ -43,4 +72,34 @@ public class event extends AppCompatActivity {
         });
     }
 
+    public void onStart() {
+        super.onStart();
+        requestQueue = Volley.newRequestQueue(this);
+
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.POST, URL, new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray jsonArray) {
+
+                for (int i = 0; i < jsonArray.length(); i++) {
+                    try {
+                        JSONObject jsonObject = jsonArray.getJSONObject(i);
+                        items.add(jsonObject.getString("name"));
+                        items.add(jsonObject.getString("location"));
+                        items.add(jsonObject.getString("date"));
+                        items.add(jsonObject.getString("time"));
+                        items.add(jsonObject.getString("description"));
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+                adapter.notifyDataSetChanged();
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Toast.makeText(getApplicationContext(), "Something went wrong.", Toast.LENGTH_SHORT).show();
+                }
+            });
+      requestQueue.add(jsonArrayRequest);
+        }
 }
